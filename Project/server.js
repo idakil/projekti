69f6044783e46a464a01c7e0e0302fc345b3ab88
Ida Kilpeläinen <ida.kilpelainen@metropolia.fi>
2019-10-02 15:44:04 +0300

    q

--- Project/server.js
+++ Project/server.js
@@ -1,8 +1,7 @@
 let express = require('express');
 let app = express();
 
-
-var server = app.listen(8081, function () {
+let server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
 
@@ -10,14 +9,26 @@ var server = app.listen(8081, function () {
 })
 
 app.use(express.static('public'));
-
 let io = require('socket.io')(server);
+let mysql = require('mysql');
 
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
+let con = mysql.createConnection({
     host: "localhost",
     user: "username",
     password: "password",
     database: "db"
+});
+
+var restaurantArr = [];
+con.connect(function (err) {
+    if (err) throw err;
+    let query = "select * from restaurants"
+    con.query(query, function (err, result, fields) {
+        if (err) throw err;
+        if (result.length) {
+            for (var i = 0; i < result.length; i++) {
+                restaurantArr.push(result[i]);
+            }
+        }
+    })
 });
