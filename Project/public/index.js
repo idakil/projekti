3586a3b7e48a3112ae2f73dac19b152aea174fa3
Ida Kilpeläinen <ida.kilpelainen@metropolia.fi>
2019-10-03 20:26:22 +0300

    eh

--- Project/public/index.js
+++ Project/public/index.js
@@ -1,29 +1,30 @@
 let socket = io();
-
 let restaurantArr = [];
 
 
 fetch("/restaurants")
     .then(r => r.json())
-    .then(function(d) {
+    .then(function (d) {
         handleRestaurants(d);
     });
 
 function handleRestaurants(d) {
-    console.log(d);
-    for (let i = 0; i < d.length; i++) {
-        restaurantArr.push(new Object());
-        restaurantArr[i].name = d[i].restaurant_name;
-    }
-    console.log(restaurantArr);
+
 }
 
 fetch("/ratings")
-    .then(r=>r.json())
-    .then(function(d) {
+    .then(r => r.json())
+    .then(function (d) {
         handleReviews(d);
     });
 
+
+
+socket.on("openPlease", handle)
+function handle(d) {
+    console.log("aaaaa")
+    console.log(d)
+}
 
 function handleReviews(d) {
     //console.log(d);
--- Project/server.js
+++ Project/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express'),
-app = express(),
-bodyParser = require('body-parser');
+  app = express(),
+  bodyParser = require('body-parser');
 port = process.env.PORT || 3000;
 
 
@@ -12,7 +12,7 @@ const mc = mysql.createConnection({
   password: 'password',
   database: 'restaurant_db',
   //port: '3308'
-}); 
+});
 mc.connect();
 
 let server = app.listen(port);
@@ -28,44 +28,60 @@ let io = require('socket.io')(server);
 io.on('connection', function (socket) {
   console.log('A user connected');
   socket.on('disconnect', function () {
-      console.log('A user disconnected');
+    console.log('A user disconnected');
   });
 });
 
 
 app.get('/restaurants', sendRestaurants);
-function sendRestaurants (req, res) {
+function sendRestaurants(req, res) {
   let query = "select * from restaurants inner join opening_hours on restaurants.id=opening_hours.restaurant_id"
   mc.query(query, function (err, result) {
-      if (err){
-          console.log("error ", err);
-      }
-      else {
-          res.send(result)
-      }
+    if (err) {
+      console.log("error ", err);
+    }
+    else {
+      res.send(result)
+    }
   })
 }
 
 
+app.get("/restaurant/:id", function (req, res) {
+  let query = "select * from restaurants where id=" + req.params.id;
+  mc.query(query, function (err, result) {
+    if (err) {
+      console.log("error ", err);
+    }
+    else {
+      socket.emit('openPlease', "viud")
+      console.log("AAAAAAAA " + result)
+    }
+  })
+})
+
+
+
+
 app.get('/ratings', sendRatings);
 
-function sendRatings (req, res) {
+function sendRatings(req, res) {
   let query = "select stars,restaurant_id from reviews"
   mc.query(query, function (err, result) {
-      if (err){
-          console.log("error ", err);
+    if (err) {
+      console.log("error ", err);
+    }
+    else {
+      let objs = [];
+      console.log(result[0].stars);
+      console.log(result);
+      let ratings = [];
+      for (let i = 0; i < result.length; i++) {
+        objs.push({ id: result[i].restaurant_id, rating: result[i].stars });
       }
-      else {
-        let objs = [];
-        console.log(result[0].stars);
-        console.log(result);
-        let ratings = [];
-        for (let i = 0;i < result.length; i++) {
-            objs.push({id: result[i].restaurant_id, rating: result[i].stars});
-        }
-        console.log(objs);
+      console.log(objs);
 
-      }
+    }
   })
 }
 
